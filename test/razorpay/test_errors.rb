ba3bc948c602466c5a35203ed5d248d0d5b1f7f4
Abhay Rana <capt.n3m0@gmail.com>
2015-03-31 19:07:35 +0530

    Fixes style issues in last commit

--- test/razorpay/test_errors.rb
+++ test/razorpay/test_errors.rb
@@ -8,14 +8,13 @@ module Razorpay
     def setup
       @payment_id = 'fake_payment_id'
       stub_get(/payments\/#{Regexp.quote(@payment_id)}$/, 'fake_payment')
-
     end
 
     def test_bad_request_error
       stub_post(%r{payments/#{@payment_id}/capture$}, 'bad_request_error', 'amount=5')
       assert_raises Razorpay::BadRequestError, 'It must raise BadRequestError' do
         payment = Razorpay::Payment.fetch(@payment_id)
-        payment.capture({amount: 5})
+        payment.capture(amount: 5)
       end
     end
   end
