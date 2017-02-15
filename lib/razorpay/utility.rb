a38db8cbb683817d93b546b9e9ecad1018852dda
Harman Singh <harman28@gmail.com>
2017-02-15 14:55:35 +0530

    [v2] Refactored utility * validate_payment_signature now accepts a hash * Implement a constant time comparison for hmac verification

--- lib/razorpay/utility.rb
+++ lib/razorpay/utility.rb
@@ -4,20 +4,35 @@ require 'openssl'
 # Helper functions are defined here
 module Razorpay
   class Utility
-    def self.validate_payment_signature(order_id, payment_id, signature)
-      return validate_signature(signature, order_id, payment_id)
+    def self.validate_payment_signature(attributes)
+      signature = attributes[:razorpay_signature]
+      order_id = attributes[:razorpay_order_id]
+      payment_id = attributes[:razorpay_payment_id]
+
+      data = [order_id, payment_id].join '|'
+
+      return validate_signature(signature, data)
     end
 
     private
 
-    def self.validate_signature(signature, *fields)
+    def self.validate_signature(signature, data)
       secret = Razorpay.auth[:password]
-
-      data = fields.join('|')
 
       expected_signature = OpenSSL::HMAC.hexdigest('SHA256', secret, data)
 
-      return expected_signature === signature
+      return secure_compare(expected_signature, signature)
+    end
+
+    def self.secure_compare(a, b)
+      return false unless a.bytesize == b.bytesize
+
+      l = a.unpack("C*")
+
+      r, i = 0, -1
+      b.each_byte { |v| r |= v ^ l[i+=1] }
+
+      return r == 0
     end
   end
 end
--- test/razorpay/test_utility.rb
+++ test/razorpay/test_utility.rb
@@ -9,13 +9,12 @@ module Razorpay
     end
 
     def test_payment_signature_validation
-      order_id = 'fake_order_id'
-      payment_id = 'fake_payment_id'
-      received_signature = 'b2335e3b0801106b84a7faff035df56ecffde06918c9ddd1f0fafbb37a51cc89'
-      valid = Razorpay::Utility.validate_payment_signature(order_id, payment_id, received_signature);
+      payment_response = { razorpay_order_id: 'fake_order_id', razorpay_payment_id: 'fake_payment_id',
+        razorpay_signature: 'b2335e3b0801106b84a7faff035df56ecffde06918c9ddd1f0fafbb37a51cc89'}
+      valid = Razorpay::Utility.validate_payment_signature(payment_response);
       assert_equal true, valid
-      received_signature = 'very_fake_signature'
-      valid = Razorpay::Utility.validate_payment_signature(order_id, payment_id, received_signature);
+      payment_response[:razorpay_signature] = '_dummy_signature_dummy_signature_dummy_signature_dummy_signature'
+      valid = Razorpay::Utility.validate_payment_signature(payment_response);
       assert_equal false, valid
     end
   end
