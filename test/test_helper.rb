b933bdb91870ba15edcdbe8942abbfd768410ce4
Harman Singh <harman28@gmail.com>
2018-09-25 11:56:08 +0530

    [rubocop] Style fixes for 0.58.2

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,6 +8,7 @@ Coveralls.wear! if ENV['CI']
 
 def fixture_file(filename)
   return '' if filename == ''
+
   file_path = File.expand_path(File.dirname(__FILE__) + '/fixtures/' + filename + '.json')
   File.read(file_path)
 end
