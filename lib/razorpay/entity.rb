b7558f6bce3010b195e3b3f2c232ce27c309800a
Nemo <me@captnemo.in>
2019-03-31 14:34:05 +0530

    [entity] Adds support for custom JSON options to to_json (#75)
    * [entity] Adds support for custom JSON options to to_json
    
    - Failing Build: https://travis-ci.org/razorpay/razorpay-ruby/jobs/513608718
    
    * [rubocop] Removes redundant braces

--- lib/razorpay/entity.rb
+++ lib/razorpay/entity.rb
@@ -28,8 +28,8 @@ module Razorpay
 
     # Public: Convert the Entity object to JSON
     # Returns the JSON representation of the Entity (as a string)
-    def to_json
-      @attributes.to_json
+    def to_json(*args)
+      @attributes.to_json(*args)
     end
 
     # Mutates the entity in accordance with
