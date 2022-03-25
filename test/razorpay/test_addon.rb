858f7fa18d95585e5a955a609968a52b7b7d3c25
ankitdas13 <ankit.das@razorpay.com>
2022-03-25 16:36:17 +0530

    added readme & assertion

--- test/razorpay/test_addon.rb
+++ test/razorpay/test_addon.rb
@@ -6,8 +6,8 @@ module Razorpay
     class Item < Razorpay::Entity; end
 
     def setup
-      @subscription_id = 'fake_subscription_id'
-      @addon_id = 'fake_addon_id'
+      @subscription_id = 'sub_00000000000001'
+      @addon_id = 'ao_IrSY3UIqDRx7df'
 
       # Any request that ends with addons/addon_id
       stub_get(%r{addons\/#{Regexp.quote(@addon_id)}$}, 'fake_addon')
@@ -22,7 +22,7 @@ module Razorpay
       assert_instance_of Razorpay::Addon, addon, 'Addon not an instance of Addon class'
       assert_equal @addon_id, addon.id, 'Addon IDs do not match'
       assert_equal 1, addon.quantity, 'Addon quantity is accessible'
-      assert_equal 'fake_subscription_id', addon.subscription_id, 'Addon subscription_id is accessible'
+      assert_equal @subscription_id, addon.subscription_id, 'Addon subscription_id is accessible'
       assert_nil addon.invoice_id, 'Addon invoice_id is accessible'
 
       assert_addon_item_details(addon)
@@ -32,7 +32,7 @@ module Razorpay
       stub_get(/addons$/, 'addon_collection')
       addons = Razorpay::Addon.all
       assert_instance_of Razorpay::Collection, addons, 'Addons should be an array'
-      assert !addons.items.empty?, 'Addon should be more than one'
+      refute_empty addons.items, 'Addon should be more than one'
     end
 
     def test_add_addons_to_subscription
@@ -44,14 +44,14 @@ module Razorpay
         quantity: 1
       }
 
-      stub_post(%r{subscriptions\/#{@subscription_id}\/addons$}, 'fake_addon', create_addon_stub_url_params)
+      stub_post(%r{subscriptions\/#{@subscription_id}\/addons$}, 'fake_addon', addon_attrs.to_json)
 
-      addon = Razorpay::Addon.create(@subscription_id, addon_attrs)
+      addon = Razorpay::Addon.create(@subscription_id, addon_attrs.to_json)
       assert_instance_of Razorpay::Addon, addon, 'Addon not an instance of Addon class'
 
-      assert_equal 'fake_addon_id', addon.id, 'Addon IDs do not match'
+      assert_equal @addon_id, addon.id, 'Addon IDs do not match'
       assert_equal 1, addon.quantity, 'Addon quantity is accessible'
-      assert_equal 'fake_subscription_id', addon.subscription_id, 'Addon subscription_id is accessible'
+      assert_equal @subscription_id, addon.subscription_id, 'Addon subscription_id is accessible'
       assert_nil addon.invoice_id, 'Addon invoice_id is accessible'
 
       assert_addon_item_details(addon)
@@ -62,18 +62,11 @@ module Razorpay
     def assert_addon_item_details(addon)
       addon_item = Item.new(addon.item)
 
-      assert_equal 'fake_item_id', addon_item.id, 'Addon Item id is accessible'
+      assert_equal 'item_00000000000001', addon_item.id, 'Addon Item id is accessible'
       assert_equal 'fake_item_name', addon_item.name, 'Addon Item name is accessible'
       assert_equal 'fake_item_description', addon_item.description, 'Addon Item description is accessible'
       assert_equal 'INR', addon_item.currency, 'Addon Item currency is accessible'
       assert_equal 500, addon_item.amount, 'Addon Item amount is accessible'
-    end
-
-    def create_addon_stub_url_params
-      %w(
-        item[name]=fake_addon_name&item[currency]=INR&item[amount]=5000&
-        item[description]=fake_addon_description&quantity=1
-      ).join
     end
   end
 end
