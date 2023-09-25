2db8208ebf5f048e7381a32479256366030c4c17
ankitdas13 <ankit.das@razorpay.com>
2023-09-25 11:51:08 +0530

    handle v1 and v2 api version

--- lib/razorpay/constants.rb
+++ lib/razorpay/constants.rb
@@ -1,6 +1,6 @@
 # Version and other constants are defined here
 module Razorpay
-  BASE_URI = 'https://api.razorpay.com/v1/'.freeze
+  BASE_URI = 'https://api.razorpay.com'.freeze
   TEST_URL = 'https://api.razorpay.com/'.freeze
   VERSION = '3.0.1'.freeze
 end
