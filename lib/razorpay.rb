ccbe78b42ac5d911806443d513fa7a4f3770a8a5
ankitdas13 <ankit.das@razorpay.com>
2023-09-27 18:02:10 +0530

    token endpoints with test & docs

--- lib/razorpay.rb
+++ lib/razorpay.rb
@@ -20,6 +20,7 @@ require 'razorpay/fund_account'
 require 'razorpay/item'
 require 'razorpay/qr_code'
 require 'razorpay/payment_method'
+require 'razorpay/token'
 
 # Base Razorpay module
 module Razorpay
